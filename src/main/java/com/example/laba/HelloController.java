7204ba0b7bface089208195105d47ad8e72478ef
YarkoevAndrey <guturgh@gmail.com>
2024-01-05 18:52:27 +0400

    lab 7

--- src/main/java/com/example/laba/HelloController.java
+++ src/main/java/com/example/laba/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.laba;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Добро пожаловать в лр 7");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button differentiation;
+
+    @FXML
+    private Button graphics;
+
+    @FXML
+    private Button operations;
+
+    @FXML
+    private Button settings;
+
+    @FXML
+    void initialize() {
+
     }
+
 }
